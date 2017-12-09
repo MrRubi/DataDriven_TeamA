e634ccac2f0ea9148fb742807e92e6c02f1f83bb
alexgonzalo <alexgonzalo2105@gmail.com>
2017-12-09 20:44:53 +0100

    rename variables and modify some comments

--- 2_Most_Attacked_Honeypot.R
+++ 2_Most_Attacked_Honeypot.R
@@ -6,6 +6,7 @@
 # [7] groucho-norcal    groucho-eu        groucho-sydney  
 # Vamos a analizar el trafico de cada uno.
 
+#include utils file for further use
 source("utils.R")
 
 #Intall needed packages (uncomment the first time of use)
@@ -23,12 +24,12 @@ library(dplyr)
 DataSet_filtered$attacks <- c(1)  # they all occur once right now
 hosts <- aggregate(attacks ~ host, data = DataSet_filtered, FUN = sum)
 
-#reorder funtion is used to print attacks in ascendir order
-gg <- ggplot(hosts, aes(x = reorder(host, attacks), y=attacks, fill = host))
-gg <- gg + geom_bar(stat = "identity", width = 0.5)
-gg <- gg + labs(y = "Attacks", x = "Honeypots", title = "Received attacks by honeypot")
-gg <- graphicCustomization(gg)
-print(gg)
+#reorder funtion is used to print attacks in ascending order
+attacksByHoneypot.graphic <- ggplot(hosts, aes(x = reorder(host, attacks), y=attacks, fill = host))
+attacksByHoneypot.graphic <- attacksByHoneypot.graphic + geom_bar(stat = "identity", width = 0.5)
+attacksByHoneypot.graphic <- attacksByHoneypot.graphic + labs(y = "Attacks", x = "Honeypots", title = "Received attacks by honeypot")
+attacksByHoneypot.graphic <- graphicCustomization(attacksByHoneypot.graphic)
+print(attacksByHoneypot.graphic)
 
 #Los honeypots con mas ataques son en orden ascendente Singapur, Oregon y Tokyo
 
@@ -38,14 +39,16 @@ print(gg)
 
 #group by host and country to know which country has performed more attacks adding the attacks entries and getting the top 3
 mostAttackedHostsByCountry <- DataSet_filtered %>% group_by(host, country) %>% summarise(attacks = sum(attacks)) %>% top_n(3, attacks)
+
+#print graphic to see the results
 dodge <- position_dodge(width = 0.9)
-gg2 <- ggplot(mostAttackedHostsByCountry, aes(x = host, y = attacks, fill = country)) 
-gg2 <- gg2 + geom_bar(stat = "identity", position = position_dodge())
-gg2 <- gg2 + labs(y = "Attacks", x = "Honeypots", title = "Top 3 attacks location")
+attacksLocation.graphic <- ggplot(mostAttackedHostsByCountry, aes(x = host, y = attacks, fill = country)) 
+attacksLocation.graphic <- attacksLocation.graphic + geom_bar(stat = "identity", position = position_dodge())
+attacksLocation.graphic <- attacksLocation.graphic + labs(y = "Attacks", x = "Honeypots", title = "Top 3 attacks location")
 
 bDelete_x_AxisNames = FALSE
-gg2 <- graphicCustomization(gg2, FALSE)
-print(gg2)
+attacksLocation.graphic <- graphicCustomization(attacksLocation.graphic, FALSE)
+print(attacksLocation.graphic)
 
 
 
